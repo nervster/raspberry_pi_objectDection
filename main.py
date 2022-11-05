ea04074eab40ad7cd39b18093767c3781b5547f9
Nirav Sheth <npsheth93@live.com>
2022-11-05 15:06:03 -0500

    test commit

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import cv2
 from picamera.array import PiRGBArray
 from picamera import PiCamera 
 thres = 0.55 # Threshold to detect object
-
+# updated comment here
 cap = cv2.VideoCapture(0)
 cap.set(3,1280)
 cap.set(4,720)
